56d99414eda7795fa2801f1b8278b7a5c64347a0
Harry Zuo <qq535321383@gmail.com>
2020-12-07 22:11:54 -0500

    Adding description for loading data

--- bikeshare.py
+++ bikeshare.py
@@ -56,7 +56,7 @@ def get_filters():
 def load_data(city, month, day):
     """
     Loads data for the specified city and filters by month and day if applicable.
-
+    Can be all or specific
     Args:
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
